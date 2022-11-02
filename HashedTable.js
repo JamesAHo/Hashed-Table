667d0cb2cc218dbf6ef40436a7f588b87329ef8e
James Anh <james.anh184@gmail.com>
2022-11-01 22:47:39 -0700

    implemented hashtable

---
+++ HashedTable.js
@@ -0,0 +1,17 @@
+// algor for hashed table
+// constructor for hased function
+class HashTable {
+    constructor(size = 7) {
+        this.dataMap = new Array(size);
+    }
+    _hash(key) {
+        let hash = 0
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i)*23) % this.dataMap.length
+        }
+        return hash
+    }
+}
+
+let myHashTable = new HashTable()
+myHashTable
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hashed-Table
 Hashed Table algo
+## About
+Hash table is a data strcuture which stores data in an associative manner. In hash table, data is stored in an array format, where each databa value has its own uniqyue index value.
