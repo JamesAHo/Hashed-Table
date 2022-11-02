c90841256adf6020b96050d18db126693a21f974
James Anh <james.anh184@gmail.com>
2022-11-01 23:26:20 -0700

    added set method

--- HashedTable.js
+++ HashedTable.js
@@ -4,6 +4,7 @@ class HashTable {
     constructor(size = 7) {
         this.dataMap = new Array(size);
     }
+    // hash function
     _hash(key) {
         let hash = 0
         for (let i = 0; i < key.length; i++) {
@@ -11,6 +12,16 @@ class HashTable {
         }
         return hash
     }
+    // set function
+    set(key, value) {
+        let index = this._hash(key)
+       if(!this.dataMap[index]) {
+        this.dataMap[index] = []
+       }
+       this.dataMap[index].push([key,value])
+       return this
+    }
+    
 }
 
 let myHashTable = new HashTable()
